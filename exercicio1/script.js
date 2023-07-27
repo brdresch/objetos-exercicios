9ea5edca9e27dbf2cab5576a0fa1b79cffe34a34
brdresch <brdresch@gmail.com>
2023-07-27 19:42:12 -0300

    Exercicio 1

---
+++ exercicio1/script.js
@@ -0,0 +1,34 @@
+
+
+const sacolao = []
+
+const banana = { 
+    nome:'banana',
+    preco:25,
+    disponivel:true
+
+}
+
+const coco = { 
+    nome:'coco',
+    preco:12,
+    disponivel:false
+
+}
+
+const maca = { 
+    nome:'maca',
+    preco:6,
+    disponivel:true
+
+}
+
+sacolao.push(banana)
+console.log(sacolao);
+
+sacolao.push(coco)
+console.log(sacolao);
+
+sacolao.push(maca)
+console.log(sacolao);
+console.log(sacolao.length);
